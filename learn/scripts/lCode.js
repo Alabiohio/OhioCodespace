9b3b5ef5483e56759e4a6648d1f51bcc0706c349
Alabiohio <ohiocheoyaalabi@gmail.com>
2025-07-11 07:06:25 +0100

    Add files via upload

---
+++ learn/scripts/lCode.js
@@ -0,0 +1,46 @@
+const runBtn = document.querySelectorAll(".run-btn");
+    runBtn.forEach(runBtn => {
+        if (runBtn) {
+            runBtn.addEventListener('click', () => {
+                const codeBox = runBtn.closest(".codeBox");
+                const codes = codeBox.querySelector('.language-html');
+                const rawHtml = codes.textContent;
+                const previewFrame = codeBox.querySelector(".tipPreview");
+                previewFrame.srcdoc = rawHtml;
+                //previewFrame.style.display = "block";
+                previewFrame.classList.toggle("show");
+                previewFrame.classList.contains("show")? runBtn.textContent = 'close' : runBtn.textContent = "run";
+            });
+        }
+    });
+    
+
+const codeBox = document.querySelectorAll(".codeBox");
+
+    
+codeBox.forEach(codeBox => {
+    const copy = document.createElement("button");
+    copy.classList.add("copy-btn");
+    copy.innerHTML = `<i class="fa fa-copy"></i>`;
+    codeBox.appendChild(copy);
+});
+
+const copyBtn = document.querySelectorAll(".copy-btn");
+
+    copyBtn.forEach(copyBtn => {
+        copyBtn.addEventListener("click", 		() => {
+            const codeBox = copyBtn.closest(".codeBox");
+            const codes = codeBox.querySelector('.language-html');
+            const rawHtml = codes.textContent;
+            navigator.clipboard.writeText(rawHtml).then(() => {
+                copyBtn.innerHTML = `<i class="fa fa-check"></i>`;
+                setTimeout( () => {
+                    copyBtn.innerHTML = `<i class="fa fa-copy"></i>`;
+                }, 3000);
+            }).catch(err => {
+                console.error("Failed to copy text: ", err);
+            });
+            
+        });
+    });
+
